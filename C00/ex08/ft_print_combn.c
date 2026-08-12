23dbf53bc9e4d34b0697c2e8ec1244fec5557bc9
Hanane AIT BAH <angelhananehanane@gmail.com>
2026-08-12 13:29:36 +0100

    Update ft_print_combn.c

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,1 +1,37 @@
+#include <unistd.h>
+
+void	afficher(int idx[9], int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		write(1, &(char){idx[i] + '0'}, 1);
+		i++;
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int	idx[9];
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		idx[i] = i;
+		i++;
+	}
+
+
+
+
+
+
+
+
+
+
+
 
